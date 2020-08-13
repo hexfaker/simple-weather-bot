2830c63e890de7922b1af4b13901c2f7185dae47
Vsevolod Poletaev <poletaev.va@gmail.com>
2020-08-13 21:02:28 +0300

    :tada: Init

---
+++ simple_weather_bot/__main__.py
@@ -0,0 +1,66 @@
+# type: ignore[attr-defined]
+
+import random
+from enum import Enum
+from typing import Optional
+
+import typer
+from rich.console import Console
+
+from simple_weather_bot import __version__
+from simple_weather_bot.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="simple-weather-bot",
+    help="Simple bot telling weather and recommending clothes",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]simple-weather-bot[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Name of person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for name. If not specified then choice will be random.",
+    ),
+    version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the simple-weather-bot package.",
+    ),
+):
+    """Prints a greeting for a giving name."""
+    if color is None:
+        # If no color specified use random value from `Color` class
+        color = random.choice(list(Color.__members__.values()))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
